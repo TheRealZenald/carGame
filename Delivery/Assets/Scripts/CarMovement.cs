d4427a13cee63484bd367ed4afb535180d0d7423
kittyLuver18 <dcayling1052@gmail.com>
2023-10-19 13:05:45 -0700

    misc

--- Delivery/Assets/Scripts/CarMovement.cs
+++ Delivery/Assets/Scripts/CarMovement.cs
@@ -4,127 +4,53 @@ using UnityEngine;
 
 public class CarMovement : MonoBehaviour
 {
-    public Transform centerOfMass;
-    public WheelCollider[] wheelColliders;
-    public Transform[] wheelVisuals;
-    public float maxMotorTorque = 4000f;
-    public float maxBrakeTorque = 1000f;
-    public float maxSteeringAngle = 30f;
-    public float handbrakeTorque = 5000f;
-    public float driftStiffness = 0.2f;
-    public ParticleSystem tireSmoke;
+    public float speed = 10f;
+    public float driftFactor = 0.9f;  // Adjust the drift intensity.
+    public float maxSteerAngle = 45f;
+
+    private float currentSpeed;
+    private float horizontalInput;
 
     private Rigidbody rb;
-    private float currentSpeed;
-    private float driftFactor;
-    private bool isDrifting = false;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
-        rb.centerOfMass = centerOfMass.localPosition;
     }
 
     private void Update()
     {
-        currentSpeed = rb.velocity.magnitude * 3.6f; // Convert m/s to km/h
-
-        CheckDrifting();
+        // Handle input for steering.
+        horizontalInput = Input.GetAxis("Horizontal");
     }
 
     private void FixedUpdate()
     {
-        float motor = maxMotorTorque * Input.GetAxis("Vertical");
-        float steering = maxSteeringAngle * Input.GetAxis("Horizontal");
+        // Calculate current speed.
+        currentSpeed = transform.InverseTransformDirection(rb.velocity).z;
 
-        if (isDrifting)
-        {
-            ApplyDriftStiffness();
-        }
+        // Apply acceleration and steering.
+        Accelerate();
+        Steer();
 
-        ApplyMotorTorque(motor);
-        ApplySteering(steering);
-        ApplyBrakes();
-
-        UpdateWheelVisuals();
+        // Apply drift factor.
+        ApplyDrift();
     }
 
-    private void CheckDrifting()
+    private void Accelerate()
     {
-        if (Input.GetButton("Fire1") && currentSpeed > 30f) // For a handbrake effect
-        {
-            isDrifting = true;
-            if (tireSmoke != null)
-            {
-                tireSmoke.Play();
-            }
-        }
-        else
-        {
-            isDrifting = false;
-            if (tireSmoke != null)
-            {
-                tireSmoke.Stop();
-            }
-        }
+        rb.AddRelativeForce(Vector3.forward * speed);
     }
 
-    private void ApplyMotorTorque(float motor)
+    private void Steer()
     {
-        foreach (WheelCollider wheel in wheelColliders)
-        {
-            wheel.motorTorque = motor;
-        }
+        float steerAngle = maxSteerAngle * horizontalInput;
+        transform.Rotate(0, steerAngle * Time.fixedDeltaTime, 0);
     }
 
-    private void ApplySteering(float steering)
+    private void ApplyDrift()
     {
-        foreach (WheelCollider wheel in wheelColliders)
-        {
-            wheel.steerAngle = steering;
-        }
-    }
-
-    private void ApplyBrakes()
-    {
-        foreach (WheelCollider wheel in wheelColliders)
-        {
-            if (isDrifting)
-            {
-                wheel.brakeTorque = handbrakeTorque;
-            }
-            else
-            {
-                wheel.brakeTorque = maxBrakeTorque;
-            }
-        }
-    }
-
-    private void ApplyDriftStiffness()
-    {
-        foreach (WheelCollider wheel in wheelColliders)
-        {
-            WheelFrictionCurve curve = wheel.sidewaysFriction;
-            curve.stiffness = driftStiffness;
-            wheel.sidewaysFriction = curve;
-        }
-    }
-
-    private void UpdateWheelVisuals()
-    {
-        for (int i = 0; i < wheelColliders.Length; i++)
-        {
-            UpdateSingleWheelVisual(wheelColliders[i], wheelVisuals[i]);
-        }
-    }
-
-    private void UpdateSingleWheelVisual(WheelCollider wheelCollider, Transform wheelVisual)
-    {
-        // Match the visual wheel's position and rotation with the WheelCollider.
-        Vector3 position;
-        Quaternion rotation;
-        wheelCollider.GetWorldPose(out position, out rotation);
-        wheelVisual.position = position;
-        wheelVisual.rotation = rotation;
+        Vector3 driftForce = -transform.right * (currentSpeed * (1 - driftFactor));
+        rb.AddForce(driftForce);
     }
 }
